9105589f5779fd5efaf593559c443e12d84fa2fb
Lzr0125 <1971861065@qq.com>
2026-03-11 22:21:30 +0800

    Update TestDemo.java

--- src/main/java/com/TestDemo.java
+++ src/main/java/com/TestDemo.java
@@ -2,6 +2,6 @@ package com;
 
 public class TestDemo {
     public static void main(String[] args) {
-        System.out.println("这是远程仓库修改的版本B");
+        System.out.println("这是远程仓库修改的版本A");
     }
 }
